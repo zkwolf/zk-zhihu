663309b22d56f6108fe80c8d241bba8bf502de4d
zkwolf <chenhao5866@gmail.com>
2015-06-03 23:53:09 +0800

    什么都没有的版本
    -、-登录还是很好登录的，编码乱码昨晚废了半个小时，今天发现挺好处理的

---
+++ fuck.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'chenhao'
+
+import requests
+import gzip
+
+s = requests.session()
+login_data = {'email': 'chenhao5866@gmail.com', 'password': 'hacker97976442'}
+s.post('http://www.zhihu.com/login', login_data)
+
+r = s.get('http://www.zhihu.com')
+with open('x.txt', 'wb') as f:
+	f.write(r.text.encode('utf-8'))
