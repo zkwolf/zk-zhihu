f98eb33dec18d7884b9a2c535a3bed934a4b716b
zkwolf <chenhao5866@gmail.com>
2015-08-10 13:02:17 +0800

    已知当关注数为0时可能出错。。。

--- followers.py
+++ followers.py
@@ -6,15 +6,11 @@ import re
 from bs4 import BeautifulSoup
 import json
 import time
-import copy
-
 
 zhihu_url = "http://www.zhihu.com"
 zhihu_login_url = zhihu_url + "/login/email"
 captcha_url = zhihu_url + "/captcha.gif?r=" + str(int(time.time()))
-data_sum = set()
-data_temp = set()
-data_next = set()
+bitch = []
 
 
 def get_chrome_cookies():
@@ -40,18 +36,18 @@ def get_chrome_cookies():
     with open('cookies', 'w') as f:
         f.write(str(ret_dict))
 
-def get_followees(url):
+
+def get_followers():
     '''
         获取关注界面，得到关注人数，确定offset的值
     '''
-    global data_temp
     if not os.path.exists('cookies'):
         get_chrome_cookies()
     with open('cookies', 'r') as f:
         cookie = eval(f.read())
     s = requests.Session()
     s.cookies.update(cookie)
-    r = s.get(url)
+    r = s.get('http://www.zhihu.com/people/zkwolf10824/followers')
     data = r.text
     raw_hash_id = re.findall('hash_id(.*)', data)
     hash_id = raw_hash_id[0][14:46]
@@ -60,8 +56,8 @@ def get_followees(url):
     soup = BeautifulSoup(data, 'html.parser')
     people = soup.select("div.zu-main-sidebar strong")
     num = int(people[0].get_text())
-    for followee in soup.select('h2.zm-list-content-title a'):
-        data_temp.add(followee.attrs.get('href'))
+    for follower in soup.select('h2.zm-list-content-title a'):
+        bitch.append(follower.attrs.get('href'))
     '''
         获取所有除第一页以外的所有关注
     '''
@@ -70,7 +66,7 @@ def get_followees(url):
     header = {
         'Host': 'www.zhihu.com',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
-        'Referer': 'http://www.zhihu.com',
+        'Referer': 'http://www.zhihu.com/people/zkwolf10824/followers',
         'X-Requested-With': 'XMLHttpRequest',
     }
     for i in range(divi):
@@ -82,42 +78,20 @@ def get_followees(url):
         params = json.dumps(
             {"offset": offset, "order_by": "created", "hash_id": hash_id})
         payload = {'method': 'next', 'params': params, '_xsrf': _xsrf}
-        request_url = "http://www.zhihu.com/node/ProfileFolloweesListV2"
+        request_url = "http://www.zhihu.com/node/ProfileFollowersListV2"
         x = s.post(request_url, data=payload, headers=header)
         temp = json.loads(x.text)
-        _followee2 = temp.get('msg')
-        followee2 = ''.join(_followee2)
-        soup = BeautifulSoup(followee2, 'html.parser')
-        for followee in soup.select('h2.zm-list-content-title a'):
-            data_temp.add(followee.attrs.get('href'))
-        time.sleep(0.5)
-
-
-def loop(depth):
-    global data_sum
-    global data_next
-    global data_temp
-    for i in range(depth):
-        if i == 0:
-            url = "http://www.zhihu.com/people/zkwolf10824/followees"
-            data_sum.add(url)
-            get_followees(url)
-            data_sum, data_temp = data_sum.union(data_temp), data_temp.difference(data_sum)
-            data_next = copy.deepcopy(data_temp)
-            data_temp.clear()
-        else:
-            print('data_next:' + str(len(data_next)))
-            for url in data_next:
-                print(url)
-                get_followees(url + '/followees')
-                time.sleep(2)
-            data_sum, data_temp = data_sum.union(data_temp), data_temp.difference(data_sum)
-            data_next = copy.deepcopy(data_temp)
-            data_temp.clear()
+        _follower2 = temp.get('msg')
+        follower2 = ''.join(_follower2)
+        soup = BeautifulSoup(follower2, 'html.parser')
+        for follower in soup.select('h2.zm-list-content-title a'):
+            bitch.append(follower.attrs.get('href'))
+    '''
+        写入txt文件，用来后续分析
+    '''
+    with open('followers.txt', 'w') as f:
+        for i in range(len(bitch)):
+            f.write(bitch[i] + '\n')
 
 if __name__ == "__main__":
-    depth = int(input('请输入深度\n'))
-    loop(depth)
-    with open('followees.txt', 'w') as f:
-        for i in data_sum:
-            f.write(i + '\n')
+    get_followers()
